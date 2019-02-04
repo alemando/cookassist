5048c473cd6c41958ae07ae8f60196b140f0c6bb
JuanCaca <47220144+JuanCaca@users.noreply.github.com>
2019-02-03 20:49:48 -0500

    Ultima Actualización: Funcionalidades

--- pedido.py
+++ pedido.py
@@ -3,76 +3,118 @@ from datetime import datetime
 class Pedido():
 
     _num_pedidos = 0
-    _tot_pedidos = {}
+    _fechas_pedidos = []
+    _nombres_pedidos = []
+    _usuarios_pedidos = []
 
-    def __init__ (self,fecha,descripcion,detalle,usuario,chef):
+    def __init__ (self,nombre="Defecto",fecha="Defecto",descripcion="Defecto",
+                detalle="Defecto",usuario="Defecto",chef="Defecto"):
         """Atributos:
+        self._nombre
         self._fecha 
         self._descripcion 
         self._ListDetalle 
         self._usuario 
         self._chef """
-        self._ListDetalle = []
-        self.setFecha(fecha) #Recordatorio: El metodo setFecha setea fecha con cualquier parametro, 
-                             #se pone en la pestaña main
-        self.setDescripcion(descripcion)
-        self.setDetalle(detalle)
-        self.setUsuario(usuario)
-        self.setChef(chef)
-        
+        self._list_detalle = []
+        self.set_nombre(nombre)
+        self.set_fecha(fecha) # Recordatorio: El metodo setFecha setea 
+                             # fecha con cualquier parametro
+        self.set_descripcion(descripcion)
+        self.set_detalle(detalle)
+        self.set_usuario(usuario)
+        self.set_chef(chef)
+
         Pedido._num_pedidos += 1
-        Pedido._tot_pedidos[self.getFecha()] = self.getUsuario()
+        Pedido._fechas_pedidos.append(self.get_fecha())
+        Pedido._nombres_pedidos.append(self.get_nombre())
+        Pedido._usuarios_pedidos.append(self.get_usuario())
 
-    def setFecha(self,fecha):
+    def set_fecha(self,fecha):
         self._fecha = datetime.now()
-        self._fecha = ("Fecha: "+str(self._fecha.day)+"-"+str(self._fecha.month)+"-"+str(self._fecha.year)+
-            " Hora: "+str(self._fecha.hour)+":"+str(self._fecha.minute)+":"+str(self._fecha.second))
+        self._fecha = ("Fecha: "+str(self._fecha.day)+"-"+
+            str(self._fecha.month)+"-"+str(self._fecha.year)+
+            " Hora: "+str(self._fecha.hour)+":"+
+            str(self._fecha.minute)+":"+str(self._fecha.second))
 
-    def getFecha(self):
+    def get_fecha(self):
         return self._fecha
 
-    def setDescripcion(self,descripcion):
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self,nombre):
+        self._nombre = nombre
+
+    def set_descripcion(self,descripcion):
         self._descripcion = descripcion
 
-    def getDescripcion(self):
+    def get_descripcion(self):
         return self._descripcion
 
-    def setDetalle(self,detalle):
-        self._ListDetalle.append(detalle)
+    def set_detalle(self,detalle):
+        self._list_detalle.append(detalle)
 
-    def getDetalle(self):
-        return self._ListeDetalle
+    def get_detalle(self):
+        return self._list_detalle
 
-    def setUsuario(self,usuario):
+    def set_usuario(self,usuario):
         self._usuario = usuario
 
-    def getUsuario(self):
+    def get_usuario(self):
         return self._usuario
 
-    def setChef(self,chef):
+    def set_chef(self,chef):
         self._chef = chef
 
-    def getChef(self):
+    def get_chef(self):
         return self._chef
 
-    def modifyAllValues(self,fecha,descripcion,detalle,usuario,chef):
-        self.setFecha(fecha)
-        self.setDescripcion(descripcion)
-        self.setDetalle(detalle)
-        self.setUsuario(usuario)
-        self.setChef(chef)
+    def modify_all_values(self,nombre,fecha,descripcion,
+                        detalle,usuario,chef):
+        self.set_nombre(nombre)
+        self.set_fecha(fecha)
+        self.set_descripcion(descripcion)
+        self.set_detalle(detalle)
+        self.set_usuario(usuario)
+        self.set_chef(chef)
 
     # Funcionalidades:   
 
     # Numero de pedidos totales
-    def getNum_pedidos(self):
+    def get_num_pedidos(self):
         return self._num_pedidos
 
-    # Pedidos totales del dia
-    def getTot_pedidos(self):
-        return self._tot_pedidos
+    # Pedidos totales del dia (fechas)
+    def get_fechas_pedidos(self):
+        for i in self._fechas_pedidos:
+            return i
+        return None
 
-    # Obetener usuario por fecha de pedido
-    # siendo 'key' una fecha
-    def getUsuario_fecha(self,key):  
-        return self._tot_pedidos.get(key) 
+    # Pedidos totales del dia (usuarios)
+    def get_usuarios_pedidos(self,key):  
+        for i in self._usuarios_pedidos:
+            return i
+        return None
+
+    # Pedidos totales del dia (nombres)
+    def get_nombres_pedidos(self,key):  
+        for i in self._nombres_pedidos:
+            return i
+        return None
+
+    # Vaciar Todos los pedidos
+    def del_tot_pedidos(self):
+        self._fechas_pedidos.clear()
+        self._nombres_pedidos.clear()
+        self._usuarios_pedidos.clear()
+        Pedido._num_pedidos = 0
+
+    # Ultimo pedido 
+    def get_ultimo_pedido(self):
+        if(Pedido._num_pedidos>0):
+            a = (str(self._nombres_pedidos[len(self._nombres_pedidos)-1])+" - "+
+            str(self._usuarios_pedidos[len(self._usuarios_pedidos)-1])+" - "+
+            str(self._fechas_pedidos[len(self._fechas_pedidos)-1]))
+            return a
+        return None
