43d229674d1645cc5620a8cf620ff5c0627e2fc9
C I R O <47220144+JuanCiroLo@users.noreply.github.com>
2019-02-16 11:27:02 -0500

    11:27

--- detallePedido.py
+++ detallePedido.py
@@ -1,44 +1,56 @@
-class detalle_pedido():
-	
-	def __init__(self,cantidad = "Defecto",pedido = "Defecto",
-				receta = "Defecto",producto = "Defecto"):
-		"""Atributos:
-		self.cantidad
-		self.pedido
-		self.receta
-		self.producto
-		"""
-		self.set_cantidad(cantidad)
-		self.set_pedido(pedido)
-		self.set_receta(receta) 
-		self.set_producto(producto)
+class Detalle_pedido():
+    
+    _list_detalle_pedido = []
 
-	def set_cantidad(self,cantidad):
-		self._cantidad = cantidad
+    def __init__(self,codigo = "Defecto",cantidad = "Defecto",pedido = "Defecto",
+                receta = "Defecto",producto = "Defecto"):
+        """Atributos:
+        self.codigo
+        self.cantidad
+        self.pedido
+        self.receta
+        self.producto
+        """
+        self.set_codigo(codigo)
+        self.set_cantidad(cantidad)
+        self.set_pedido(pedido)
+        self.set_receta(receta) 
+        self.set_producto(producto)
+        Detalle_pedido._list_detalle_pedido.append(self)
 
-	def get_cantidad(self):
-		return self._cantidad
+    def set_codigo(self,codigo):
+        self._codigo = codigo
 
-	def set_pedido(self,pedido):
-		self._pedido = pedido
+    def get_codigo(self):
+        return self._codigo
 
-	def get_pedido(self):
-		return self._pedido
+    def set_cantidad(self,cantidad):
+        self._cantidad = cantidad
 
-	def set_receta(self,receta):
-		self._receta = receta
+    def get_cantidad(self):
+        return self._cantidad
 
-	def get_receta(self):
-		return self._receta
+    def set_pedido(self,pedido):
+        self._pedido = pedido
 
-	def set_producto(self,producto):
-		self._producto = producto
+    def get_pedido(self):
+        return self._pedido
 
-	def get_producto(self):
-		return self._producto
+    def set_receta(self,receta):
+        self._receta = receta
 
-	def delete_detalle_pedido(self):
-		self.set_cantidad(None)
-		self.set_pedido(None)
-		self.set_receta(None) 
-		self.set_producto(None)
+    def get_receta(self):
+        return self._receta
+
+    def set_producto(self,producto):
+        self._producto = producto
+
+    def get_producto(self):
+        return self._producto
+
+    def delete_detalle_pedido(self):
+        self.set_cantidad(None)
+        self.set_pedido(None)
+        self.set_receta(None) 
+        self.set_producto(None)
+
