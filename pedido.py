350fc65c5c8e1591cd6b983f744ec8d49327d110
C I R O <47220144+JuanCiroLo@users.noreply.github.com>
2019-02-18 13:44:32 -0500

    Actualizado

--- pedido.py
+++ pedido.py
@@ -6,45 +6,49 @@ class Pedido():
     _num_pedidos_eliminados = 0
     _list_pedidos = []
 
-    def __init__ (self,nombre="Defecto",fecha="Defecto",descripcion="Defecto",
-                detalle="Defecto",usuario="Defecto",chef="Defecto"):
+    def __init__ (self,descripcion,detalle,usuario,entregado = False):
         """Atributos:
         self._nombre
         self._fecha 
         self._descripcion 
         self._ListDetalle 
         self._usuario 
-        self._chef """
+        self._chef 
+        self._entregado (estado)
+        """
         self._list_detalle = []
-        self.set_nombre(nombre)
-        self.set_fecha(fecha) # Recordatorio: El metodo setFecha setea 
-                             # fecha con cualquier parametro
-        self.set_descripcion(descripcion)
+        self.set_codigo(Pedido._num_pedidos)
+        self.set_fecha() # Recordatorio: El metodo set_fecha setea 
+                              # fecha con cualquier parametro
+        self.set_estado(entregado)
         self.set_detalle(detalle)
+        self.set_descripcion(descripcion)
         self.set_usuario(usuario)
-        self.set_chef(chef)
 
         Pedido._num_pedidos += 1
         Pedido._list_pedidos.append(self)
 
-    def set_fecha(self,fecha):
-        if(fecha != None):
+    def set_fecha(self):
             self._fecha = datetime.now()
             self._fecha = ("Fecha: "+str(self._fecha.day)+"-"+
                 str(self._fecha.month)+"-"+str(self._fecha.year)+
                 " Hora: "+str(self._fecha.hour)+":"+
                 str(self._fecha.minute)+":"+str(self._fecha.second))
-        else:
-            self._fecha = None
+
+    def get_estado(self):
+        return self._entregado
+
+    def set_estado(self,entregado):
+        self._entregado = entregado
 
     def get_fecha(self):
         return self._fecha
 
-    def get_nombre(self):
-        return self._nombre
+    def get_codigo(self):
+        return self._codigo
 
-    def set_nombre(self,nombre):
-        self._nombre = nombre
+    def set_codigo(self,codigo):
+        self._codigo = codigo
 
     def set_descripcion(self,descripcion):
         self._descripcion = descripcion
@@ -71,7 +75,7 @@ class Pedido():
         return self._chef
 
     def delete_all_values(self):
-        self.set_nombre(None)
+        self.set_codigo(None)
         self.set_fecha(None)
         self.set_descripcion(None)
         self.set_detalle(None)
@@ -89,15 +93,20 @@ class Pedido():
     def del_tot_pedidos(self):
         Pedido._list_pedidos.clear()
 
-    # Ultimo pedido 
-    def get_ultimo_pedido(self):
+    # retorna Primer pedido no entregado de la lista
+    @staticmethod
+    def get_primer_pedido():
         if(len(Pedido._list_pedidos)>0):
-            return Pedido._list_pedidos[len(Pedido._list_pedidos)-1]
+            for ped in Pedido._list_pedidos:
+                if ped.get_estado == False :
+                    return ped
+                    break
+        return None
 
     # Modificar todo
     def modify_all_values(self,nombre,fecha,descripcion,
                         detalle,usuario,chef):
-        self.set_nombre(nombre)
+        self.set_codigo(nombre)
         self.set_fecha(fecha)
         self.set_descripcion(descripcion)
         self.set_detalle(detalle)
